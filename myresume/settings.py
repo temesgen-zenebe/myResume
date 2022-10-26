acf5ed13ff24712e46214bb30bf1ca4e9d9ca550
temesgen-zenebe <temf2006@gmail.com>
2022-10-25 20:57:15 -0700

    python manage.py migrate

--- myresume/settings.py
+++ myresume/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
 import django_on_heroku    
-django_on_heroku.settings(locals())
+#django_on_heroku.settings(locals())
 django_on_heroku.settings(locals(),staticfiles=False)
